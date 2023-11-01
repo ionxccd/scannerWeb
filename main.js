e659d65b5767c4a600ed36441cf663bd4cb7fd68
xccd <64030555+ionxccd@users.noreply.github.com>
2023-11-01 15:20:51 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerText = "dw"
+document.getElementById("demo").innerHTML = "dw"
+
+let name = "dw";
+
+myFunction(){
+    document.getElementByID("demo").innerHTML = "dw";
+}
