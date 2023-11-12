c8fb5c3305218dcbfc2408b2408800d3d3c76eac
xccd <64030555+ionxccd@users.noreply.github.com>
2023-11-11 19:48:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,28 +4,6 @@ const currentDateTime = moment();
 const formattedDateTime = currentDateTime.format("MM/DD/YYYY"); 
 document.getElementById("random").textContent = formattedDateTime;
 
-const tabLinks = document.querySelectorAll('.tab-link');
-const tabContents = document.querySelectorAll('.tab-content');
-
-tabLinks[1].classList.add('active');
-tabContents[1].classList.add('active');
-
-
-// Add click event listeners to tab links
-tabLinks.forEach((link, index) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    // Remove the "active" class from all tab links and tab contents
-    tabLinks.forEach((tabLink) => tabLink.classList.remove('active'));
-    tabContents.forEach((tabContent) => tabContent.classList.remove('active'));
-
-    // Add the "active" class to the clicked tab link and corresponding tab content
-    link.classList.add('active');
-    tabContents[index].classList.add('active');
-  });
-});
-
 const start = () => {
   gapi.client.init({
     'apiKey': 'AIzaSyDsgL-6Gag7-UZPcby23wB0DDhcCJub_KE',
@@ -66,9 +44,50 @@ const start = () => {
   }).catch((err) => {
       console.log(err.error.message);
   });
-
-
-  
 };
+
+function getUser(event){
+  event.preventDefault();
+  gapi.client.init({
+    'apiKey': 'AIzaSyDsgL-6Gag7-UZPcby23wB0DDhcCJub_KE',
+    'discoveryDocs': ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+  }).then(() => {
+    return gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: '1FPOuToN-Xdun8cBq1dW2cFt3CgJFifQxvjFBf313FVQ',
+      range: 'A:D', // for example: List 1!A1:B6
+    });
+  }).then((response) => {
+    // parse the response data
+    const loadedData = response.result.values;
+  
+    // populate the HTML table with the data
+    const table = document.getElementsByTagName('table')[0];
+  
+    // add column headers
+    const columnHeaders = document.createElement('tr');
+    columnHeaders.innerHTML = `<th>Student ID</th>
+      <th>Student Name</th><th>Date</th>`;
+    table.appendChild(columnHeaders);
+  
+    // add table data rows
+    let idToSearch = document.getElementById('eventSearch').value;
+    let count = 0;
+  
+    for (let i = loadedData.length - 1; i >= 1; i--) {
+      if (loadedData[i][0] === idToSearch) {
+        count++;
+      }
+    }
+  
+    // Display the count in console or wherever you need
+    document.getElementById('nameCount').textContent = `Name count: ${count}`;
+  }).catch((err) => {
+    console.log(err.error.message);
+  });  
+}
+
+function testing(){
+  console.log("jtknwkdjwn");
+}
 
 gapi.load('client', start);
